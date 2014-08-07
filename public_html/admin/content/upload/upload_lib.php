0ecb3a1d605ffb13fb4256df30910aad51eb1207
Wolfgang Faust <wolfgang42@git>
2014-08-07 19:26:04 +0000

    Upload creates the relevant directories in cache/ if they do not exist.

--- public_html/admin/content/upload/upload_lib.php
+++ public_html/admin/content/upload/upload_lib.php
@@ -1,6 +1,8 @@
 <?php
 require_once('../../../../includes.php');
 define('UPLOAD_CACHE', BASE_DIR.'/cache/upload/');
+foreach (array(UPLOAD_CACHE,UPLOAD_CACHE.'/dest',UPLOAD_CACHE.'/tmp') as $dir)
+	if (!file_exists($dir)) mkdir($dir);
 
 // TODO: I don't actually recall why there are two slightly different requirements for identifiers and filenames...
 function upload_sanitize_identifier($identifier) {
