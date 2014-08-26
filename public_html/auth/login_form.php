85242ec4e486cbcdfc229b8e6dd0d3b7182c03d6
Wolfgang Faust <wolfgang42@git>
2014-08-26 19:37:59 +0000

    Update login form to show message on cookie, rather than $_GET

--- public_html/auth/login_form.php
+++ public_html/auth/login_form.php
@@ -2,5 +2,5 @@
 require_once('../../inc_unauthenticated.php');
 parse_str($_SERVER['REDIRECT_QUERY_STRING'],$REDIRECT_GET);
 $twig->display('page/auth/login_form.twig', array(
-	'loggedout' => (isset($REDIRECT_GET['msg'])&& ($REDIRECT_GET['msg'] == 'logout'))
+	'loggedout' => isset($_COOKIE['loggedout'])
 ));
